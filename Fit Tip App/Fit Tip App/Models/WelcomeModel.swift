f4d0ce4b3b4331591fbdbf67648028797485b82c
Atheer <athos@outlook.sa>
2021-11-23 20:57:11 +0300

    Welcome Page, Firebase configuration

---
+++ Fit Tip App/Fit Tip App/Models/WelcomeModel.swift
@@ -0,0 +1,16 @@
+//
+//  WelcomeModel.swift
+//  Fit Tip App
+//
+//  Created by Atheer Othman on 18/04/1443 AH.
+//
+
+import UIKit
+
+struct Welcome {
+    
+    let imageName: String
+    let topic: String
+    let description: String
+    
+}
---
+++ Fit Tip App/Pods/Target Support Files/Pods-Fit Tip App/Pods-Fit Tip App-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Fit_Tip_App : NSObject
+@end
+@implementation PodsDummy_Pods_Fit_Tip_App
+@end
